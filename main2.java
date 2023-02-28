ee2e1c0d73de3a26dbf5ffd03f5332d5a36d29b2
sONdza <menkina042019@gmail.com>
2023-03-01 00:19:27 +0300

    Типы целых чисел

---
+++ main2.java
@@ -0,0 +1,25 @@
+package ru.javalearning;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+		
+	//Типы целых чисел
+
+        {
+            //int g = ( (a + b) / c);
+
+            // a+ ((b+c);
+
+            long x = 100000;
+
+            //Int размер 32 бита, int должен быть не более 2^31, иначе выдаст ошибку
+            //long размер 64 бита
+
+            System.out.println((int) (x * x) );
+            System.out.println( x );
+        }
+	}
+}
